b8fc6b6c70569e4c4ddeb7d3ba841d22263719d7
Yago Dunka <yago.dunka@alunos.sc.senac.br>
2023-11-27 20:00:41 -0300

    Api

--- src/main/java/com/example/restapi/controllers/ProdutoController.java
+++ src/main/java/com/example/restapi/controllers/ProdutoController.java
@@ -1,6 +1,8 @@
 package com.example.restapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class ProdutoController {
 	@GetMapping("/produtos")
 	public String getProdutos() {
 		return "produtos";
+	}
+	
+	@PostMapping("/produtos")
+	public String salvarProduto(@RequestBody String contato) {
+		return contato;
 	}
 
 }
